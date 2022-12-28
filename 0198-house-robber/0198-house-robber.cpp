2b4492068864ea5b7c25a981b0fcb4516692a93b
Utkarsh Jain <utkarshjain433@gmail.com>
2022-12-28 08:50:15 -0600

    Time: 0 ms (100.00%), Space: 7.8 MB (49.98%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        } else if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        } else if (nums.size() == 3) {
+            return max(nums[0]+nums[2], nums[1]);
+        }
+        nums[2] += nums[0];
+        for (int i=3; i<nums.size(); i++) {
+            nums[i] = max(nums[i]+nums[i-2], nums[i]+nums[i-3]);
+        }
+        return max(nums[nums.size()-1], nums[nums.size()-2]);
+    }
+};
