5ee92be0d69288658f4227697a67e3fd537ca894
Utkarsh Jain <utkarshjain433@gmail.com>
2023-01-03 11:38:40 -0600

    Time: 56 ms (31.77%), Space: 51.1 MB (19.33%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void calculate(int i, int n, int k, vector<int> vec, vector<vector<int>>& res) {
+        if (k == 0) {
+            res.push_back(vec);
+            return;
+        }
+        if (i+k-1 > n) {
+            return;
+        }
+        if (i > n) {
+            return;
+        }
+        calculate(i+1, n, k, vec, res);
+        vec.push_back(i);
+        calculate(i+1, n, k-1, vec, res);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> vec;
+        vector<vector<int>> res;
+        calculate(1, n, k, vec, res);
+        return res;
+    }
+};
