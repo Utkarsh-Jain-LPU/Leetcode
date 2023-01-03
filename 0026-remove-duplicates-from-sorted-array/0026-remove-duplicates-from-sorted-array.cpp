cdb82735af49eb0604a83886d4b9b53ddfde6a66
Utkarsh Jain <utkarshjain433@gmail.com>
2023-01-03 10:57:53 -0600

    Time: 19 ms (66.86%), Space: 18.5 MB (39.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,11 +5,11 @@ public:
         if (n == 0 || n == 1) return n;
         int i = 0, j = 1;
         while (j < n) {
-            if (nums[i] == nums[j]) j++;
-            else {
+            if (nums[i] != nums[j]) {
                 nums[i+1] = nums[j];
-                i++, j++;
+                i++;
             }
+            j++;
         }
         return i+1;
     }
