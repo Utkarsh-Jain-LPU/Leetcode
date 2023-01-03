adc4b1a8b9b9880be9a0ee33a781eb7cfed58b20
Utkarsh Jain <utkarshjain433@gmail.com>
2023-01-03 11:39:02 -0600

    Time: 91 ms (21.11%), Space: 51.2 MB (19.12%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -5,12 +5,8 @@ public:
             res.push_back(vec);
             return;
         }
-        if (i+k-1 > n) {
-            return;
-        }
-        if (i > n) {
-            return;
-        }
+        if (i+k-1 > n) return;
+        if (i > n) return;
         calculate(i+1, n, k, vec, res);
         vec.push_back(i);
         calculate(i+1, n, k-1, vec, res);
