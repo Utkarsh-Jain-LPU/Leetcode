a005f2d839d27a96f87ce26d40d0197a26bd85a4
Utkarsh Jain <utkarshjain433@gmail.com>
2023-01-08 06:53:50 -0600

    Time: 32 ms (91.33%), Space: 12.5 MB (70.67%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& pt) {
-        int ans=1; 
-        int n=pt.size();
-       // slope of every point with selected point
-        for(int i=0;i<n-1;i++){
+        int ans = 1, n = pt.size();
+        for (int i=0; i<n-1; i++) {
             map<double,int> mp;
-            for(int j=i+1;j<n;j++){
-                //calculating the slope
-                double x = (double)(pt[j][1]-pt[i][1])/(double)(pt[j][0]-pt[i][0]);
-                if(pt[j][1]-pt[i][1]<0 &&(pt[j][0]-pt[i][0])==0 ) //infinite slop for Perpendicular line
-                mp[abs(x)]++; 
-                else
-               //storing the slop into map
-                    mp[x]++;
+            for (int j=i+1; j<n; j++) {
+                double x = (double)(pt[j][1]-pt[i][1])/(pt[j][0]-pt[i][0]);
+                if (pt[j][1] < pt[i][1] && pt[j][0] == pt[i][0]) mp[abs(x)]++; 
+                else mp[x]++;
             }
-            // same slope w.r.t.that selected point
             int temp = 0;
-            for(auto it:mp)
-                temp = max(temp, it.second+1);    // +1 for the current point(point itself)
+            for(auto it : mp) temp = max(temp, it.second + 1);
             ans = max(temp, ans);
         }
         return ans;
